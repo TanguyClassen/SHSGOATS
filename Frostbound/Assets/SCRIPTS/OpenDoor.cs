58961c4468998302f3290ecb659019d95c190ab8
thomasheng <tom.heng05@gmail.com>
2026-05-18 13:37:35 +0200

    vrverver

--- Frostbound/Assets/SCRIPTS/OpenDoor.cs
+++ Frostbound/Assets/SCRIPTS/OpenDoor.cs
@@ -5,6 +5,7 @@ public class OpenDoor : MonoBehaviour
     public Animator animator;
     public AudioSource audioSource;
     public AudioClip doorSound;
+    public GameObject highlight; // ← ajoute ça
 
     private bool isOpen = false;
 
@@ -16,6 +17,23 @@ public class OpenDoor : MonoBehaviour
             animator.SetTrigger("Ouvrir");
             if (audioSource != null && doorSound != null)
                 audioSource.PlayOneShot(doorSound);
+
+            // Désactive le highlight après l'ouverture
+            Invoke("DisableHighlight", 1f); // 1f = durée de l'animation
+            Invoke("DisableColliders", 1f);
         }
+    }
+
+    void DisableHighlight()
+    {
+        if (highlight != null)
+            highlight.SetActive(false);
+    }
+
+    void DisableColliders()
+    {
+        // Désactive les colliders des portes
+        foreach (Collider col in GetComponentsInChildren<Collider>())
+            col.enabled = false;
     }
 }
